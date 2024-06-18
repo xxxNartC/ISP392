6a5d5cc40cec0de224688e7184ecf7512a3f97b2
quan3 <daominhquan166@gmail.com>
2024-06-18 18:41:40 +0700

    them RES

---
+++ src/java/Controllers/SearchReservationServlet.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import DAL.PreOrderDAO;
+import Model.PreOrder;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "SearchReservationServlet", urlPatterns = {"/SearchReservationServlet"})
+public class SearchReservationServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String searchName = request.getParameter("searchName");
+        
+        PreOrderDAO dao = new PreOrderDAO();
+        List<PreOrder> reservations = dao.searchReservationsByName(searchName);
+
+        request.setAttribute("reservations", reservations);
+        request.getRequestDispatcher("searchResults.jsp").forward(request, response);
+    }
+}
